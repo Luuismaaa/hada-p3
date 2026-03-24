6c54ed8f0e6fe095a94b27dd670bec8bb770c9c4
Cesar Caballero Lloret <ccl78@alu.ua.es>
2026-03-24 17:28:02 +0100

    24437006N Creados atributos con sus propiedades en ENProduct

--- library/ENProduct.cs
+++ library/ENProduct.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace library
 {
-    internal class ENProduct
+    public class ENProduct
     {
+        private string _code;
+        private string _name;
+        private int _amount;
+        private float _price;
+        private int _category;
+        private DateTime _creationDate;
+
+
+        public string Code { get => _code; set => _code = value; }
+        public string Name { get => _name; set => _name = value; }
+        public int Amount { get => _amount; set => _amount = value; }
+        public float Price { get => _price; set => _price = value; }
+        public int Category { get => _category; set => _category = value; }
+        public DateTime CreationDate { get => _creationDate; set => _creationDate = value; }
+
+
+
     }
 }
