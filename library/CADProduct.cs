e09ec73e0b2d9a6d1b9a24de6a624b542acf0253
Cesar Caballero Lloret <ccl78@alu.ua.es>
2026-03-24 18:59:12 +0100

    24437006N Añadidos metodos update y delete en CADProduct

--- library/CADProduct.cs
+++ library/CADProduct.cs
@@ -55,5 +55,74 @@ namespace library
 
             return success;
         }
+
+        public bool update(ENProduct en)
+        {
+            bool success = false;
+            SqlConnection c = new SqlConnection(constring);
+
+            try
+            {
+                c.Open();
+                string sql = "UPDATE Products SET name = @name, amount = @amount, price = @price, " +
+                             "category = @category, creationDate = @creationDate WHERE code = @code";
+
+                SqlCommand com = new SqlCommand(sql, c);
+
+                com.Parameters.AddWithValue("@name", en.Name);
+                com.Parameters.AddWithValue("@amount", en.Amount);
+                com.Parameters.AddWithValue("@price", en.Price);
+                com.Parameters.AddWithValue("@category", en.Category);
+                com.Parameters.AddWithValue("@creationDate", en.CreationDate);
+                com.Parameters.AddWithValue("@code", en.Code);
+
+                if(com.ExecuteNonQuery() > 0)
+                {
+                    success = true;
+                }
+            }
+            catch (SqlException ex)
+            {
+                success = false;
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+            }
+            finally
+            {
+                c.Close();
+            }
+            return success;
+        }
+
+        public bool delete(ENProduct en)
+        {
+            bool success = false;
+            SqlConnection c = new SqlConnection(constring);
+
+            try
+            {
+                c.Open();
+                // Borramos usando el Code
+                string sql = "DELETE FROM Products WHERE code = @code";
+
+                SqlCommand com = new SqlCommand(sql, c);
+                com.Parameters.AddWithValue("@code", en.Code);
+
+                if (com.ExecuteNonQuery() > 0)
+                {
+                    success = true;
+                }
+            }
+            catch (SqlException ex)
+            {
+                success = false;
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+            }
+            finally
+            {
+                c.Close();
+            }
+
+            return success;
+        }
     }
 }
